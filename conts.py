a622f58a74de521aaf5a955912c59ce534b32d19
cameron <cameron@cmcgarry.co.uk>
2019-11-21 11:35:03 +0000

    Bring in CaF constants

---
+++ conts.py
@@ -0,0 +1,10 @@
+# Physical constants
+
+# Mass of CaF molecule (kg)
+m_CaF = 9.7907E-26
+
+# Rotational constant of CaF molecule (Hz)
+B_CaF = 1.02675E10
+
+# Electric diple moment of CaF molecule (mass * Hz/V)
+u_CaF = 1.54E4
