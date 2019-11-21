8a0bbb615ae17c63581342e53cdb1d6af565ccb4
cameron <cameron@cmcgarry.co.uk>
2019-11-21 13:53:38 +0000

    Tests for Wire

---
+++ test.py
@@ -0,0 +1,48 @@
+import unittest
+import numpy as np
+
+from wire import WireSegment
+
+class TestWireMethods(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Initialise a wire for basic testing
+        """
+        self.start = [-1, 0, 0]
+        self.end = [1, 0, 0]
+        self.current = 1
+        self.wire = WireSegment(self.start, self.end, self.current)
+
+    def tearDown(self):
+        pass
+
+    def test_wire_properties(self):
+        """
+        Basic testing of the wire property functions and that initialisation ran
+        correctly
+        """
+        np.testing.assert_array_equal(self.wire.start, np.array(self.start))
+        np.testing.assert_array_equal(self.wire.end, np.array(self.end))
+        self.assertEqual(self.wire.current, self.current)
+
+    def test_field(self):
+        """
+        Test the field method.
+
+        This is a simple test case, direction should be in z, and the magnitude
+        can be found analytically
+        """
+        mu0 = 1
+        r = [0, 1, 0]
+        magnitude = mu0 / (2 * np.sqrt(2) * np.pi)
+        z_hat = np.array([0, 0, 1])
+        np.testing.assert_array_almost_equal(self.wire.field(r),
+                                             magnitude * z_hat
+                                             )
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+
