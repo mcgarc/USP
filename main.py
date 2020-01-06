11b638cdda81bd7943dce850c6d358c1827e3f7d
cameron <cameron@cmcgarry.co.uk>
2020-01-06 16:14:30 +0000

    Move integrator inside Particle class

--- main.py
+++ main.py
@@ -8,33 +8,16 @@ from utils import grad
 import scipy.integrate as integ
 import numpy as np
 
-
-def Q_dot(t, Q, potential):
-    """
-    Return the time derivative of Q, the 6-vector position in momentum space,
-    for a particle in given potential at time t. (Hamilton's equations)
-    """
-    r = Q[:3]
-    v = Q[3:]
-    dvx_dt = -grad(potential, t, r, 'x')
-    dvy_dt = -grad(potential, t, r, 'y')
-    dvz_dt = -grad(potential, t, r, 'z')
-    dQ_dt = np.array([
-            v[0],
-            v[1],
-            v[2],
-            dvx_dt,
-            dvy_dt,
-            dvz_dt
-            ])
-    return dQ_dt
     
 
 def main():
+    """
+    Test simulation of a single particle in a rising potential
+    """
     mol = particle.Particle(
             [0, 0, 0.11],
             [0, 0, 0],
-            1E-20,
+            1
             )
     cur = parameter.ConstantParameter(1)
     #height = parameter.ConstantParameter(0.1)
@@ -44,14 +27,12 @@ def main():
             zcluster,
             height
             )
-    # RK45 takes a function with args t and Q, so pass in potential here
-    Q_dot_wpot = lambda t, Q: Q_dot(t, Q, ztrap.potential)
     t_end = 100000
     max_step = t_end/1000
-    ans = integ.RK45(Q_dot_wpot, 0, mol.Q, t_end, max_step=max_step)
-    while ans.t < t_end:
-        ans.step()
-        print(ans.y[:3])
+    mol.init_integ(ztrap.potential, t_end, max_step=max_step)
+    while mol.t < t_end:
+        mol.step_integ()
+        print('{}: {}'.format(mol.t, mol.r))
 
 
 if __name__ == '__main__':
--- particle.py
+++ particle.py
@@ -1,15 +1,25 @@
 import numpy as np
+import scipy.integrate as integ
+
+from utils import grad
 
 
 class Particle:
     """
-    Contain information about a particle
+    Describes a particle with spatial position, velocity and mass. Particle also
+    has the attribute _integ, which is the integrator to describe its propagation
+    in time, and the attribute _terminated, which flags if the particle has
+    been destroyed or otherwise lost
     """
 
     def __init__(self, r, v, m):
         self.set_r(r)
+        self._r_0 = self.r
         self.set_v(v)
+        self._v_0 = self.v
         self.set_m(m)
+        self._integ = None
+        self._terminated = False
 
     @property
     def r(self):
@@ -22,6 +32,13 @@ class Particle:
     @property
     def m(self):
         return self._m
+
+    @property
+    def t(self):
+        if self._integ is None:
+            return None
+        else:
+            return self._integ.t
 
     @property
     def Q(self):
@@ -47,3 +64,58 @@ class Particle:
 
     def set_m(self, m):
         self._m = m
+
+    def init_integ(
+            self,
+            potential,
+            t_end,
+            t_0=0,
+            max_step=float('inf')
+            ):
+        """
+        Initialise the integrator for the particle. Takes the potential (which
+        we expect to be a function of t and r) and a boundary time. Can take the
+        initial time (assumed zero) and a maximum allowed step (assumed inf)
+        """
+        # Construct dQ_dt, which is to be stepped by the integrator
+        dQ_dt = lambda t, Q: self._dQ_dt(t, Q, potential)
+        self._integ = integ.RK45(dQ_dt, t_0, self.Q, t_end, max_step=max_step)
+
+    def step_integ(self):
+        """
+        """
+        if not self._terminated:
+            self._integ.step()
+            # Update particle position and momentum with stepped values
+            Q = self._integ.y
+            self.set_r(Q[:3])
+            self.set_v(Q[3:])
+        # Check for termination
+        self.check_termination()
+
+    def _dQ_dt(self, t, Q, potential):
+        """
+        Return the time derivative of Q, the 6-vector position in momentum space,
+        for a particle in given potential at time t. (Hamilton's equations)
+        """
+        r = Q[:3]
+        v = Q[3:]
+        dvx_dt = -grad(potential, t, r, 'x') / self.m
+        dvy_dt = -grad(potential, t, r, 'y') / self.m
+        dvz_dt = -grad(potential, t, r, 'z') / self.m
+        dQ_dt = np.array([
+                v[0],
+                v[1],
+                v[2],
+                dvx_dt,
+                dvy_dt,
+                dvz_dt
+                ])
+        return dQ_dt
+
+    def check_termination(self):
+        """
+        TODO: Check if the particle must be terminated, for example due to
+        violating some boundary condition (like touching the surface of a chip!)
+        """
+        pass
