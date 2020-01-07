91023e4b72540fbbbbc4b92c1a668e9af8ccbd26
cameron <cameron@cmcgarry.co.uk>
2020-01-07 11:06:08 +0000

    Test simulate multiple particles

--- main.py
+++ main.py
@@ -12,27 +12,42 @@ import numpy as np
 
 def main():
     """
-    Test simulation of a single particle in a rising potential
+    Test simulation of a few particles in a rising potential
     """
-    mol = particle.Particle(
-            [0, 0, 0.11],
-            [0, 0, 0],
-            1
-            )
+
+    # Initialise particles
+    particle_no = 3
+    mols = []
+    for _ in range(particle_no):
+        mol = particle.Particle(
+                [0, 0, 0.1 + np.random.normal(0,0.01)],
+                [0, 0, 0],
+                1
+                )
+        mols.append(mol)
+
+    # Initialise potential
     cur = parameter.ConstantParameter(1)
-    #height = parameter.ConstantParameter(0.1)
     height = parameter.RampParameter([0.1, 0.2], [-2, -1, 50, 1050, float('inf'), float('inf')])
     zcluster = wire.ZWire(cur, 0.1)
     ztrap = trap.ClusterTrap(
             zcluster,
             height
             )
+
+    # Initialise integrators
     t_end = 100000
     max_step = t_end/1000
-    mol.init_integ(ztrap.potential, t_end, max_step=max_step)
-    while mol.t < t_end:
-        mol.step_integ()
-        print('{}: {}'.format(mol.t, mol.r))
+    for mol in mols:
+        mol.init_integ(ztrap.potential, t_end, max_step=max_step)
+
+    # Stepper
+    for step in range(1000):
+        for mol in mols:
+            mol.step_integ()
+        positions = np.array([ mol.r for mol in mols ])
+        times = [ mol.t for mol in mols ] 
+        print('{}: {}'.format(times, positions.mean(axis=0)))
 
 
 if __name__ == '__main__':
