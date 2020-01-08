ca38e6aac46112e4f5524cf80ff1a15bbbc6c485
cameron <cameron@cmcgarry.co.uk>
2020-01-08 14:05:19 +0000

    Fix utils.clean_vector length bug and tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,11 +13,11 @@ class TestUtils(unittest.TestCase):
         """
         vect = [1, 2, 3]
         clean = utils.clean_vector(vect)
-        self.assertEqual(type(clean), np.array)
+        self.assertEqual(type(clean), np.ndarray)
         np.testing.assert_array_equal(np.array(vect), clean)
         self.assertRaises(ValueError, utils.clean_vector, vect, 4)
 
         # Not 3-vectors
         vect_2 = [1, 2]
-        clean_2 = utils.clean_vector(vect_2)
+        clean_2 = utils.clean_vector(vect_2, 2)
         np.testing.assert_array_equal(vect_2, clean_2)
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def clean_vector(vector, length=3):
     Ensure a vector is of the right length and format before setting it
     """
     vector = np.array(vector)
-    if len(vector) != 3:
+    if len(vector) != length:
         raise ValueError(f'Vector should have length {length}')
     return vector
 
