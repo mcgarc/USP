aae6edf31a643e4303bb1ada6d1c966c865ba05c
cameron <cameron@cmcgarry.co.uk>
2020-02-05 11:15:12 +0000

    Harmonic example and animate examples

--- example_qp.py
+++ example_qp.py
@@ -11,16 +11,19 @@ def main():
     qp_trap = trap.FieldTrap(qp.field)
 
     # Simulation
-    t_end = 100
+    t_end = 10
     sim = simulation.Simulation(4, qp_trap, 0, t_end, 1E-2)
     sim.init_particles(100, 1, 1E-1, 1E-3)
+    start = time.time()
     sim.run()
+    print('runtime: {}'.format(time.time() - start))
 
     print(sim.get_total_energy(0))
     print(sim.get_total_energy(t_end))
 
     # Visualisation
-    sim.plot_start_end_positions()
+    #sim.plot_start_end_positions()
+    sim.animate()
 
 if __name__ == '__main__':
     main()
