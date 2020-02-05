6cb65d31207672c23599021bcb9908018123a925
cameron <cameron@cmcgarry.co.uk>
2020-02-05 16:08:53 +0000

    Move Zeeman functionality into trap class

--- USP/trap.py
+++ USP/trap.py
@@ -4,6 +4,7 @@ from USP.wire import WireCluster
 from USP.field import StaticField
 
 from USP import parameter
+from USP import consts
 
 class AbstractPotentialTrap:
     """
@@ -19,27 +20,27 @@ class AbstractPotentialTrap:
         # TODO Check that potential_function is of the correct form
         self.potential_function = potential_function
 
-    def potential(self, r, t):
+    def potential(self, t, r):
         """
         """
-        return self.potential_function(r, t)
+        return self.potential_function(t, r)
 
 class AbstractTrap:
 
     def __init__(self):
         pass
 
-    def field(self, r, t):
+    def field(self, t, r):
         """
         Return the trap field at position r and time t
         """
         raise NotImplementedError
 
-    def potential(self, r, t):
+    def potential(self, t, r):
         """
         Return the trap potential at position r and time t
         """
-        return np.linalg.norm(self.field(r, t))
+        return np.linalg.norm(self.field(t, r))
 
 class FieldTrap(AbstractTrap):
     """
@@ -49,6 +50,100 @@ class FieldTrap(AbstractTrap):
 
     def __init__(self, field):
         self.field = field
+
+class ZeemanGuide(AbstractPotentialTrap):
+    """
+    Zeeman guiding potential
+    """
+
+    def __init__(
+            self,
+            B0str = 1.05,
+            B1strX = 1.25,
+            B0strY = 1.0,
+            B0weak = 0.05,
+            B1weakX = 0.3,
+            B1weakY = 0.5,
+            B2weakX = 0.04,
+            B2weakY = 0.05,
+            L = 2E-2,
+            R = 2.5E-3
+            ):
+        """
+        I took the necessary formulae from Gautam's Mathematica notebook,
+        so I don't really understand what any of these mean.
+        """
+        self.B0str = B0str 
+        self.B1strX = B1strX
+        self.B0strY = B0strY
+        self.B0weak = B0weak
+        self.B1weakX = B1weakX
+        self.B1weakY = B1weakY
+        self.B2weakX = B2weakX
+        self.B2weakY = B2weakY
+        self.L = L
+        self.R = R
+
+
+    def axial_B_sin(self, z, L):
+        z = float(z)
+        L = float(L)
+        return np.sin(np.pi*z/L)**2
+    
+    def radial_strong_B(self, rho, B0, B1, R):
+        rho = float(rho)
+        B0 = float(B0)
+        B1 = float(B1)
+        R = float(R)
+        return (B1 - B0)*rho**2/R**2
+    
+    def radial_weak_B(self, rho, B0, B1, B2, R):
+        rho = float(rho)
+        B0 = float(B0)
+        B1 = float(B1)
+        B2 = float(B2)
+        R = float(R)
+        # a soln
+        det_a = B0*B1 - B0*B2 - B1*B2 + B2**2
+        # ignore very small values (floating point happens)
+        if det_a < 1E-10:
+            det_a = 0
+        a = ((B0 + B1 - 2*B2)*R**4 + 2*np.sqrt(det_a)*R**4)/R**8
+        # b soln
+        det_b = (B1-B2)*(B0-B2)
+        if det_b < 0:
+            print(B0, B1, B2, det_b)
+        b = 2*(B2 - B0 - np.sqrt(det_b))/R**2
+        return a*rho**4 + b*rho**2
+    
+    
+    def potential(self, t, r):
+        x = r[0]
+        y = r[1]
+        z = r[2]
+        B0str = 1.05
+        pot = self.axial_B_sin(z, self.L) * (
+                self.radial_strong_B(x, self.B0str, self.B1strX, self.R) +
+                self.radial_strong_B(y, self.B0str, self.B0strY, self.R) +
+                self.B0str
+                ) + self.axial_B_sin(z + self.L/2, self.L) * (
+                self.radial_weak_B(
+                    x,
+                    self.B0weak,
+                    self.B1weakX,
+                    self.B2weakX,
+                    self.R
+                    ) +
+                self.radial_weak_B(
+                    y,
+                    self.B0weak,
+                    self.B1weakY,
+                    self.B2weakY,
+                    self.R
+                    ) +
+                self.B0weak
+                )
+        return pot * consts.u_B
 
 class ClusterTrapStatic(AbstractTrap):
     """
--- example_zeeman.py
+++ example_zeeman.py
@@ -2,77 +2,15 @@ from USP import *
 import numpy as np
 import time
 
-def harmonic_2d_potential(t, r):
-    V_0 = 0
-    m = 1
-    omega_0 = 1
-    return V_0 + 0.5 * m * omega_0 * (r[1]*r[1] + r[2]*r[2])
-
-def axial_B_sin(z, L):
-    z = float(z)
-    L = float(L)
-    return np.sin(np.pi*z/L)**2
-
-def radial_strong_B(rho, B0, B1, R):
-    rho = float(rho)
-    B0 = float(B0)
-    B1 = float(B1)
-    R = float(R)
-    return (B1 - B0)*rho**2/R**2
-
-def radial_weak_B(rho, B0, B1, B2, R):
-    rho = float(rho)
-    B0 = float(B0)
-    B1 = float(B1)
-    B2 = float(B2)
-    R = float(R)
-    # a soln
-    det_a = B0*B1 - B0*B2 - B1*B2 + B2**2
-    # ignore very small values (floating point happens)
-    if det_a < 1E-10:
-        det_a = 0
-    a = ((B0 + B1 - 2*B2)*R**4 + 2*np.sqrt(det_a)*R**4)/R**8
-    # b soln
-    det_b = (B1-B2)*(B0-B2)
-    if det_b < 0:
-        print(B0, B1, B2, det_b)
-    b = 2*(B2 - B0 - np.sqrt(det_b))/R**2
-    return a*rho**4 + b*rho**2
-
-
-def zeeman_potential(t, r):
-    x = r[0]
-    y = r[1]
-    z = r[2]
-    B0str = 1.05
-    B1strX = 1.25
-    B0strY = 1.
-    B0weak = 0.05
-    B1weakX = 0.3
-    B1weakY = 0.5
-    B2weakX = 0.04
-    B2weakY = 0.05
-    L = 2E-2
-    R = 2.5E-3
-    pot = axial_B_sin(z, L) * (
-            radial_strong_B(x, B0str, B1strX, R) +
-            radial_strong_B(y, B0str, B0strY, R) +
-            B0str
-            ) + axial_B_sin(z + L/2, L) * (
-            radial_weak_B(x, B0weak, B1weakX, B2weakX, R) +
-            radial_weak_B(y, B0weak, B1weakY, B2weakY, R) +
-            B0weak
-            )
-    return pot * consts.u_B
 
 
 def main():
     """
-    Test simulation of a few particles in a rising potential
+    Test simulation of a few particles in a Zeeman guide
     """
 
-    # Initialise harmonic trap
-    h_trap = trap.AbstractPotentialTrap(zeeman_potential)
+    # Initialise Zeeman guide
+    z_guide = trap.ZeemanGuide()
 
     # Simulation
     t_end = 10E-3
@@ -82,7 +20,7 @@ def main():
     T = 4
     v_spread = np.sqrt(2*consts.k_B*T/mass)
     v_z = 144
-    sim = simulation.Simulation(4, h_trap, 0, t_end, 1E-6)
+    sim = simulation.Simulation(4, z_guide, 0, t_end, 1E-6)
     sim.init_particles(100, mass, r_spread, v_spread, v_centre=[0, 0, v_z])
     sim.run()
     print(f'runtime: {sim.run_time}')
