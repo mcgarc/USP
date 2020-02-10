a4ec96157a3ff52bde189139756a4a978be53883
cameron <cameron@cmcgarry.co.uk>
2020-02-10 17:11:52 +0000

    Translate qp with sigmoid displacement

--- example_qp_t.py
+++ example_qp_t.py
@@ -7,11 +7,21 @@ class QuadrupoleFieldMoving(field.QuadrupoleField):
     Adapt quadrupole field to move in time
     """
 
-    def D_r(self, t):
+    def D_r(self, t, t_start=4, t_end=14, r_shift=0.1, v=1):
         """
         Change in r over time
         """
-        return np.array([0, 0, t/10.])
+        t_start = float(t_start)
+        t_end = float(t_end)
+        r_shift = float(r_shift)
+        if t < t_start:
+            return np.array([0, 0, 0])
+        elif t > t_end:
+            return np.array([0, 0, r_shift])
+        else:
+            denom = 1 + np.exp(-v * (t - (t_start + t_end)/2))
+            r = r_shift / denom
+            return np.array([0, 0, r])
 
     def field(self, t, r):
         r = np.array(r) - self.r_0 - self.D_r(t)
@@ -34,7 +44,7 @@ def main():
     v_spread = np.sqrt(2*consts.k_B*T/mass)
  
     # Simulation
-    t_end = 1
+    t_end = 20
     sim = simulation.Simulation(qp_trap, 0, t_end, 1E-3)
     sim.init_particles(50, mass, r_spread, v_spread)
     sim.run()
@@ -45,8 +55,8 @@ def main():
     sim.plot_start_end_positions()
     sim.plot_temperatures()
     xylim = (-1E-2, 1E-2)
-    zlim = (-1E-3, 0.101)
-    sim.animate(1000, xlim=xylim, ylim=xylim, zlim=zlim)#, output_path='results/qp.mp4')
+    zlim = (-1E-3, 0.121)
+    sim.animate(2000, xlim=xylim, ylim=xylim, zlim=zlim, output_path='results/qp_move.mp4')
 
 if __name__ == '__main__':
     main()
