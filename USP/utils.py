7fe3b136106a99fa359280013f775d9623760d80
cameron <cameron@cmcgarry.co.uk>
2020-02-11 13:47:11 +0000

    utils.clean_direction_index

--- USP/utils.py
+++ USP/utils.py
@@ -19,14 +19,7 @@ def grad(f, t, r, direction, delta=DX):
     Returns the gradient of a function f = f(t, r) in the specified direction
     """
     r = np.array(r)
-    if direction == 'x' or direction == 0:
-        i = 0
-    elif direction == 'y' or direction == 1:
-        i = 1
-    elif direction == 'z' or direction == 2:
-        i = 2
-    else:
-        raise ValueError('Unexpected gradient direction')
+    i = clean_direction_index(direction)
     direction = [0, 0, 0]
     direction[i] = 1
     delta_plus = r + delta * np.array(direction)
@@ -57,3 +50,32 @@ def create_output_csv(times, mol_Qs):
             for Q in Qs:
                 # TODO Index
                 writer.writerow(Q)
+
+def clean_direction_index(dir_index, str_rep=False):
+    """
+    Take a direction index and ensure it is valid. e.g. input 0, 1, 2
+    corresponds to x, y and z directions. No other input allowed.
+
+    Args:
+    dir_index: int-castable or str, the direction to be cleaned
+    str_rep: bool, whether to return the string representation of direction too
+    """
+    # Recast floats as ints
+    if type(dir_index) in [float, np.float64]:
+        dir_index = int(dir_index)
+    # Get correct index
+    if dir_index == 'x' or dir_index == 0:
+        i = 0
+        s = 'x'
+    elif dir_index == 'y' or dir_index == 1:
+        i = 1
+        s = 'y'
+    elif dir_index == 'z' or dir_index == 2:
+        i = 2
+        s = 'z'
+    else:
+        raise ValueError('Unexpected direction index')
+    # Return index (and string representation)
+    if str_rep:
+        return i, s
+    return i
