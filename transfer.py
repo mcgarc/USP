c94ea92659f96ac60521967d3aa265e12be52e18
cameron <cameron@cmcgarry.co.uk>
2020-03-27 15:30:13 +0000

    Transfer test script: QP -> U

---
+++ transfer.py
@@ -0,0 +1,114 @@
+from USP import *
+import time
+import numpy as np
+import os
+
+def main():
+    """
+    Test simulation of particles transferring between traps
+    """
+
+    # Setup output directory
+    start_time_str = time.strftime("%Y-%m-%dT%H:%M:%S")
+    path = f'results/{start_time_str}/'
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    # Save script file with results
+    with open(__file__, 'r') as f:
+        copy_script_path = f'{path}runscipt.py'
+        with open(copy_script_path, 'w') as f_out:
+            for line in f:
+                f_out.write(line)
+            f_out.close()
+        f.close()
+
+    # Initialise U wire
+    # Thick wire should be able to take 100A
+    I = 300
+    I = parameter.RampParameter([consts.u_B * I], [2E-3, 5E-3, 1, 1.1])
+    z_0 = 1E-3
+    z_1 = 1E-6
+    #I = parameter.ConstantParameter(consts.u_B * I)
+    #h = parameter.SigmoidParameter(z_0, z_1, 4E-3, 6E-3, 1E-3)
+    h = parameter.ConstantParameter(z_0)
+    u_wire = wire.UWire(I, z_0)
+    u_trap = trap.ClusterTrap(u_wire, h, bias_scale=[0, 1, 0])
+
+    # Initialise QP
+    b_1_0 = consts.u_B * .6
+    b_1 = parameter.RampParameter([b_1_0], [-1, 0, 2E-3, 5E-3])
+    qp = field.QuadrupoleField(b_1, r_0=[0, 0, z_0])
+    qp_trap = trap.FieldTrap(qp.field)
+
+    # Combination trap
+    combi_trap = trap.SuperimposeTrap([qp_trap, u_trap])
+
+    # Initial conditions
+    T = 50E-6
+    mass = consts.m_Rb
+    r_spread = .5E-4
+    v_spread = np.sqrt(2*consts.k_B*T/mass)
+
+    # Particle loss
+    limit = 20*r_spread
+    loss_event = events.OutOfRangeBox(limit, center=[0, 0, z_0])
+ 
+    # Simulation
+    POINTS = 400
+    t_end = 10E-3
+    dt = 0.5E-6
+    PARTICLES = 12
+    r_centre = [0., 0., z_0]
+    sim = simulation.Simulation(
+            combi_trap,
+            0,
+            t_end,
+            dt,
+            POINTS,
+            events=loss_event
+            )
+    sim.init_particles(PARTICLES, mass, r_spread, v_spread, r_centre=r_centre)
+
+    sim.run()
+
+    E_0 = sim.get_total_energy(0)
+    E_end = sim.get_total_energy(-1)
+    print(f'Energy differential: {(E_0 - E_end) / E_0:.5f} %')
+    
+    sim.plot_temperatures(output_path=f'{path}temps.png')
+    sim.plot_particle_number(output_path=f'{path}particle_no.png')
+    sim.plot_center(0, output_path=f'{path}center_x.png')
+    sim.plot_center(1, output_path=f'{path}center_y.png')
+    sim.plot_center(2, output_path=f'{path}center_z.png')
+    sim.plot_width(0, output_path=f'{path}width_x.png')
+    sim.plot_width(1, output_path=f'{path}width_y.png')
+    sim.plot_width(2, output_path=f'{path}width_z.png')
+    sim.plot_velocity_width(0, output_path=f'{path}velocity_width_x.png')
+    sim.plot_velocity_width(1, output_path=f'{path}velocity_width_y.png')
+    sim.plot_velocity_width(2, output_path=f'{path}velocity_width_z.png')
+    sim.plot_cloud_volume(output_path=f'{path}volume.png')
+    sim.plot_cloud_phase_space_volume(output_path=f'{path}ps_volum.png')
+    sim.plot_position_histogram(0, 0, output_path=f'{path}r_t0_hist_x.png')
+    sim.plot_position_histogram(0, 1, output_path=f'{path}r_t0_hist_y.png')
+    sim.plot_position_histogram(0, 2, output_path=f'{path}r_t0_hist_z.png')
+    sim.plot_position_histogram(-1, 0, output_path=f'{path}r_t1_hist_x.png')
+    sim.plot_position_histogram(-1, 1, output_path=f'{path}r_t1_hist_y.png')
+    sim.plot_position_histogram(-1, 2, output_path=f'{path}r_t1_hist_z.png')
+    sim.plot_momentum_histogram(0, 0, output_path=f'{path}p_t0_hist_x.png')
+    sim.plot_momentum_histogram(0, 1, output_path=f'{path}p_t0_hist_y.png')
+    sim.plot_momentum_histogram(0, 2, output_path=f'{path}p_t0_hist_z.png')
+    sim.plot_momentum_histogram(-1, 0, output_path=f'{path}p_t1_hist_x.png')
+    sim.plot_momentum_histogram(-1, 1, output_path=f'{path}p_t1_hist_y.png')
+    sim.plot_momentum_histogram(-1, 2, output_path=f'{path}p_t1_hist_z.png')
+    #xlim = (-3*z_0, 3*z_0)
+    #ylim = (-3*z_0, 3*z_0)
+    #zlim = (xlim[0], xlim[1] + z_0)
+    xlim=(-3*r_spread, 3*r_spread)
+    ylim = xlim
+    zlim = (xlim[0], xlim[1] + z_0)
+    sim.animate(xlim=xlim, ylim=ylim, zlim=zlim, output_path=f'{path}anim.mp4')
+
+
+if __name__ == '__main__':
+    main()
