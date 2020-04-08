c49f9411239f70aef20d8e7712aacc334fcea29d
cameron <cameron@cmcgarry.co.uk>
2020-04-08 23:21:34 +0100

    Setup new transfer test

--- transfer.py
+++ transfer.py
@@ -8,15 +8,9 @@ def main():
     Test simulation of particles transferring between traps
     """
 
-    # Setup output directory
-    start_time_str = time.strftime("%Y-%m-%dT%H:%M:%S")
-    path = f'results/{start_time_str}/'
-    if not os.path.exists(path):
-        os.mkdir(path)
-
     # Save script file with results
     with open(__file__, 'r') as f:
-        copy_script_path = f'{path}runscipt.py'
+        copy_script_path = f'runscipt.py'
         with open(copy_script_path, 'w') as f_out:
             for line in f:
                 f_out.write(line)
@@ -26,13 +20,10 @@ def main():
     # Initialise U wire
     # Thick wire should be able to take 100A
     I = 10 * consts.u_B
-    #I = parameter.RampParameter([consts.u_B * I], [2E-3, 5E-3, 1, 1.1])
     I = parameter.RampParameter([I], [2E-3, 5E-3, 1, 1.1])
     z_0 = 1E-3
     z_1 = 1E-6
-    #I = parameter.ConstantParameter(consts.u_B * I)
-    #h = parameter.SigmoidParameter(z_0, z_1, 4E-3, 6E-3, 1E-3)
-    h = parameter.ConstantParameter(z_0)
+    h = parameter.SigmoidParameter(z_0, z_1, 8E-3, 12E-3, 1E-3)
     u_wire = wire.UWire(I, z_0)
     u_trap = trap.ClusterTrap(u_wire, h, bias_scale=[0, 1, 1])
 
@@ -55,21 +46,11 @@ def main():
     limit = 10E-3
     loss_event = events.OutOfRangeBox(limit, center=[0, 0, z_0])
 
-#    u_trap.plot_potential_cut(
-#            1,
-#            0.5E-3,
-#            2*z_0,
-#            'z',
-#            cut_point=[0,0,z_0]
-#            #output_path=f'{path}_pot_plot_x'
-#            )
-#    quit()
- 
     # Simulation
     POINTS = 400
     t_0 = 0
-    t_end = 1E-3
-    dt = 0.5E-6
+    t_end = 15E-3
+    dt = 1E-6
     PARTICLES = 4
     r_centre = [0., 0., z_0]
     sim = simulation.Simulation(
@@ -85,7 +66,7 @@ def main():
 
     sim.run()
 
-    sim.pickle('results/output.pickle')
+    sim.pickle(prepend_path='results/')
 
 
 if __name__ == '__main__':
