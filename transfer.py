892bd67b43a79b1eb98f4e03b613a01ff4bb7b6e
cameron <cameron@cmcgarry.co.uk>
2020-04-22 12:09:09 +0100

    transfer setup uses new center

--- transfer.py
+++ transfer.py
@@ -19,39 +19,44 @@ def main():
 
     # Initialise U wire
     # Thick wire should be able to take 100A
-    I = 10 * consts.u_B
-    I = parameter.RampParameter([I], [2E-3, 5E-3, 1, 1.1])
+    I = 100 * consts.u_B
+    I = parameter.RampParameter([I], [100E-3, 200E-3, 100, 200])
+    u_axis = 10E-3
+    y_0 = 3E-3
+    y_1 = 0
+    y = parameter.SigmoidParameter(y_0, y_1, 100E-3, 200E-3, 50)
     z_0 = 1E-3
-    z_1 = 1E-6
-    h = parameter.SigmoidParameter(z_0, z_1, 8E-3, 12E-3, 1E-3)
-    u_wire = wire.UWire(I, z_0)
-    u_trap = trap.ClusterTrap(u_wire, h, bias_scale=[0, 1, 1])
+    z_1 = 0.1E-3
+    z = parameter.SigmoidParameter(z_0, z_1, 300E-3, 400E-3, 50)
+    u_wire = wire.UWire(I, u_axis)
+    u_trap = trap.ClusterTrapStatic(u_wire)
 
     # Initialise QP
     b_1_0 = consts.u_B * .6
-    b_1 = parameter.RampParameter([b_1_0], [-1, 0, 2E-3, 5E-3])
+    b_1 = parameter.RampParameter([b_1_0], [-1, 0, 100E-3, 200E-3])
     qp = field.QuadrupoleField(b_1, r_0=[0, 0, z_0])
     qp_trap = trap.FieldTrap(qp.field)
 
     # Combination trap
-    combi_trap = trap.SuperimposeTrap([qp_trap, u_trap])
+    center = parameter.ArrayParameter([0, y, z])
+    combi_trap = trap.SuperimposeTrapWBias([qp_trap, u_trap], center)
 
     # Initial conditions
     T = 50E-6
     mass = consts.m_Rb
-    r_spread = 0.05E-3
+    r_spread = 5E-3
     v_spread = np.sqrt(2*consts.k_B*T/mass)
 
     # Particle loss
-    limit = 10E-3
+    limit = 100E-3
     loss_event = events.OutOfRangeBox(limit, center=[0, 0, z_0])
 
     # Simulation
-    POINTS = 400
+    POINTS = 200
     t_0 = 0
-    t_end = 15E-3
+    t_end = 500E-3
     dt = 1E-6
-    PARTICLES = 4
+    PARTICLES = 3
     r_centre = [0., 0., z_0]
     sim = simulation.Simulation(
             combi_trap,
