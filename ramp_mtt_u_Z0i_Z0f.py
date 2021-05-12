eab16fd88fb580d009d876d0c79a99db4a0e7538
Cameron McGarry <cameron@cmcgarry.co.uk>
2021-05-12 15:55:54 +0100

    Update ramp script

--- ramp_mtt_u_Z0i_Z0f.py
+++ ramp_mtt_u_Z0i_Z0f.py
@@ -31,29 +31,28 @@ mass = consts.m_CaF
 # Initialise QP
 b_1_0 = consts.u_B * .1 # Levitation graident 10G/cm
 b_1_1 = consts.u_B * .6
-b_1 = parameter.RampParameter([b_1_0, b_1_1], [-2, -1, t_m2, t_m1, t_1, t_2])
+b_1 = parameter.RampParameterUpDown2(-2, -1, t_m2, t_m1, t_1, t_2, b_1_0,  b_1_1)
 qp = field.QuadrupoleField(b_1, r_0=[0, 0, 3E-3])
 qp_trap = trap.FieldTrap(qp.field)
 
 # Initialise U wire
 I_1 = -100 * consts.u_B
-I = parameter.RampParameter([I_1], [t_1, t_2, t_3, t_4])
+I = parameter.RampParameterUpDown(t_1, t_2, t_3, t_4, I_1)
 u_axis = 16E-3
 u_wire = wire.UWire(I, u_axis, center=[0,0,-4E-3])
-#B_y = parameter.RampParameter([-22E-4*consts.u_B], [t_1, t_2, 1, 2])
-#B_z = parameter.RampParameter([-14E-4*consts.u_B], [t_1, t_2, 1, 2])
-#bias = parameter.ArrayParameter([0, B_y, B_z])
 u_trap = trap.ClusterTrapStatic(u_wire)
 
 # Initialise Z wire
 I_z_0 = -30 * consts.u_B
-I_z = parameter.RampParameter([I_z_0], [t_3, t_4, 1, 2])
+I_z = parameter.RampParameterUpDown(t_3, t_4, 1, 2, I_z_0)
 z_axis = 12E-3
 z_wire = wire.ZWireXAxis(I_z, z_axis)
 z_trap = trap.ClusterTrapStatic(z_wire)
 
 # Combination trap
-height = parameter.RampParameter([3E-3, 1E-3], [-2, -1, t_5, t_6, 2, 3])
+h_0 = 3E-3
+h_1 = 1E-3
+height = parameter.RampParameterUpDown2(-2, -1, t_5, t_6, 2, 3, h_0, h_1)
 center = [0, 0, height]
 center = parameter.ArrayParameter(center)
 combi_trap = trap.SuperimposeTrapWBias([qp_trap, u_trap, z_trap], center, bias_scale=[0.95, 1, 1])
@@ -61,19 +60,18 @@ combi_trap = trap.SuperimposeTrapWBias([qp_trap, u_trap, z_trap], center, bias_s
 # Particle generators
 l = 1E-3
 r_gen = rand.NormalGenerator([0, 0, 3E-3], [l, l, l])
-#v_gen = rand.TemperatureGenerator(mass, 100E-6, length=3)
 T = 50E-6
 v_gen = rand.TemperatureGeneratorNormal(mass, T, length=3)
 
 # Particle loss (if particle touches the chip or reaches a limit in a cardinal direction)
 limit = 10E-3
-loss_event = events.OutOfRangeBox(-limit, -limit, -limit, limit, limit, limit)
+loss_event = events.OutOfRangeBox(-limit, -limit, 0, limit, limit, limit)
 
 # Simulation
 POINTS = 200
 t_end = t_7
 dt = 1E-4
-PARTICLES = 1E3
+PARTICLES = 300
 sim = simulation.Simulation(
         combi_trap,
         t_m3,
@@ -81,10 +79,10 @@ sim = simulation.Simulation(
         dt,
         POINTS,
         events=loss_event,
-        process_no=32
+        process_no=8
         )
 sim.init_particles(PARTICLES, mass, r_gen, v_gen)
 
 sim.run()
 
-sim.pickle(path='output.pickle')
+sim.pickle(path=f'results/ramp_dt{dt}_N{PARTICLES}_T{T*1E6}uK.pickle')
